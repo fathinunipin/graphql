259c348de3facd32cf7e8a44c29f1557a1786357
Philipp Zängerling <dev@pzaenger.com>
2022-08-23 01:52:49 +0200

    Added first query to list all films

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { retry, Subscription, timeout } from 'rxjs';
+import { FindAllFilmsGQL, FindAllFilmsQuery } from '../graphql/generated';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  loading = false;
+  allFilms: FindAllFilmsQuery['allFilms'] | undefined;
+
+  private allFilmsSubscription: Subscription | undefined;
+
+  constructor(private findAllFilmsGQL: FindAllFilmsGQL) {
+  }
+
+  ngOnInit(): void {
+    this.allFilmsSubscription = this.findAllFilmsGQL.watch().valueChanges
+      .pipe(timeout(5_000), retry(3))
+      .subscribe({
+        next: ({ data, loading }) => {
+          this.loading = loading;
+          this.allFilms = data.allFilms;
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.allFilmsSubscription) this.allFilmsSubscription.unsubscribe();
+  }
 }
